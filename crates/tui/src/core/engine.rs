Hmbown/DeepSeek-TUI

feat(tui): route shell and file tools through typed permission rules

crates/tui/src/core/engine.rs
@@ -166,6 +166,9 @@ pub struct EngineConfig {
     pub search_provider: crate::config::SearchProvider,
     /// API key for Tavily or Bocha. `None` for DuckDuckGo.
     pub search_api_key: Option<String>,
+    /// Typed permission rules used before falling back to the legacy approval
+    /// prompt path for shell and file tools.
+    pub exec_policy_engine: deepseek_execpolicy::ExecPolicyEngine,
 }
 
 impl Default for EngineConfig {
@@ -206,6 +209,7 @@ impl Default for EngineConfig {
             workshop: None,
             search_provider: crate::config::SearchProvider::default(),
             search_api_key: None,
+            exec_policy_engine: deepseek_execpolicy::ExecPolicyEngine::default(),
         }
     }
 }
@@ -1947,10 +1951,11 @@ use self::approval::{ApprovalDecision, ApprovalResult, UserInputDecision};
 use self::dispatch::should_parallelize_tool_batch;
 use self::dispatch::{
     ParallelToolResult, ParallelToolResultEntry, ToolExecGuard, ToolExecOutcome,
-    ToolExecutionBatch, ToolExecutionPlan, caller_allowed_for_tool, caller_type_for_tool_use,
-    final_tool_input, format_tool_error, mcp_tool_approval_description, mcp_tool_is_parallel_safe,
-    mcp_tool_is_read_only, parse_parallel_tool_calls, parse_tool_input,
+    ToolExecutionBatch, ToolExecutionPlan, ToolPermissionOverride, caller_allowed_for_tool,
+    caller_type_for_tool_use, final_tool_input, format_tool_error, mcp_tool_approval_description,
+    mcp_tool_is_parallel_safe, mcp_tool_is_read_only, parse_parallel_tool_calls, parse_tool_input,
     plan_tool_execution_batches, should_force_update_plan_first, should_stop_after_plan_tool,
+    tool_permission_override_for_call,
 };
 use self::loop_guard::{AttemptDecision, LoopGuard, OutcomeDecision};
 #[cfg(test)]

crates/tui/src/main.rs
@@ -8,6 +8,7 @@ use std::time::Duration;
 use anyhow::{Context, Result, anyhow, bail};
 use clap::{Args, CommandFactory, Parser, Subcommand, ValueEnum};
 use clap_complete::{Shell, generate};
+use deepseek_execpolicy::PermissionDecision;
 use dotenvy::dotenv;
 use tempfile::NamedTempFile;
 use wait_timeout::ChildExt;
@@ -4185,6 +4186,11 @@ fn merge_project_config(config: &mut Config, workspace: &Path) {
         config.allow_shell = Some(v);
     }
 
+    // Project-level permission config is allowed only when it tightens the
+    // effective policy. A repository can require extra prompts or denials, but
+    // it cannot grant itself broader tool access.
+    merge_project_permission_config(config, table);
+
     // #454: instructions array — project replaces user. Empty arrays
     // count: explicit `instructions = []` clears the user's list for
     // this repo, useful when the user has a verbose global file that
@@ -4198,6 +4204,72 @@ fn merge_project_config(config: &mut Config, workspace: &Path) {
             .collect();
         config.instructions = Some(entries);
     }
+}
+
+fn merge_project_permission_config(
+    config: &mut Config,
+    table: &toml::map::Map<String, toml::Value>,
+) {
+    if let Some(value) = table.get("auto_allow") {
+        let ignored = value.as_array().map_or(0, Vec::len);
+        if ignored > 0 {
+            eprintln!(
+                "warning: project-scope `auto_allow` is ignored — \
+                 project config cannot grant persistent command allow rules."
+            );
+        }
+    }
+
+    if let Some(value) = table.get("auto_deny") {
+        match string_array_from_project_value(value) {
+            Some(entries) => config.auto_deny.extend(entries),
+            None => eprintln!(
+                "warning: project-scope `auto_deny` is ignored — expected an array of strings."
+            ),
+        }
+    }
+
+    let Some(value) = table.get("permissions") else {
+        return;
+    };
+    let permissions: crate::config::PermissionsConfig = match value.clone().try_into() {
+        Ok(permissions) => permissions,
+        Err(err) => {
+            eprintln!(
+                "warning: project-scope `[permissions]` is ignored — failed to parse permission rules: {err}"
+            );
+            return;
+        }
+    };
+
+    for rule in permissions.rules {
+        match rule.decision {
+            PermissionDecision::Deny | PermissionDecision::Ask => {
+                config.permissions.rules.push(rule);
+            }
+            PermissionDecision::Allow => {
+                eprintln!(
+                    "warning: project-scope allow permission rule `{}` is ignored — \
+                     project config cannot grant persistent tool allow rules.",
+                    rule.pattern_label()
+                );
+            }
+        }
+    }
+}
+
+fn string_array_from_project_value(value: &toml::Value) -> Option<Vec<String>> {
+    Some(
+        value
+            .as_array()?
+            .iter()
+            .map(toml::Value::as_str)
+            .collect::<Option<Vec<_>>>()?
+            .into_iter()
+            .filter(|item| !item.trim().is_empty())
+            .map(str::to_string)
+            .collect(),
+    )
 }
 
 async fn run_interactive(
@@ -4612,6 +4684,7 @@ async fn run_exec_agent(
             .and_then(|s| s.provider)
             .unwrap_or_default(),
         search_api_key: config.search.as_ref().and_then(|s| s.api_key.clone()),
+        exec_policy_engine: config.exec_policy_engine(),
     };
 
     let engine_handle = spawn_engine(engine_config, config);
@@ -5643,6 +5716,48 @@ allow_shell = false
         merge_project_config(&mut config, tmp.path());
         assert_eq!(config.max_subagents, Some(4));
         assert_eq!(config.allow_shell, Some(false));
+    }
+
+    #[test]
+    fn project_overlay_applies_only_tightening_permission_rules() {
+        let tmp = workspace_with_project_config(
+            r#"
+auto_allow = ["cargo test"]
+auto_deny = ["rm -rf"]
+
+[[permissions.rules]]
+tool = "read_file"
+decision = "ask"
+path = "secrets/**"
+
+[[permissions.rules]]
+tool = "write_file"
+decision = "deny"
+path = "src/**"
+
+[[permissions.rules]]
+tool = "exec_shell"
+decision = "allow"
+command = "cargo test"
+"#,
+        );
+        let mut config = Config::default();
+        merge_project_config(&mut config, tmp.path());
+
+        assert!(
+            config.auto_allow.is_empty(),
+            "project-scope auto_allow must not grant access"
+        );
+        assert_eq!(config.auto_deny, ["rm -rf"]);
+        assert_eq!(config.permissions.rules.len(), 2);
+        assert_eq!(
+            config.permissions.rules[0].decision,
+            PermissionDecision::Ask
+        );
+        assert_eq!(
+            config.permissions.rules[1].decision,
+            PermissionDecision::Deny
+        );
     }
 
     #[test]

social4hyq/CodeWhale

crates/tui/src/core/engine.rs
@@ -171,6 +171,9 @@ pub struct EngineConfig {
     /// once at engine construction, then threaded onto every
     /// `SubAgentRuntime` the engine builds (#1806, #1808).
     pub subagent_api_timeout: Duration,
+    /// Typed permission rules used before falling back to the legacy approval
+    /// prompt path for shell and file tools.
+    pub exec_policy_engine: codewhale_execpolicy::ExecPolicyEngine,
 }
 
 impl Default for EngineConfig {
@@ -214,6 +217,7 @@ impl Default for EngineConfig {
             subagent_api_timeout: Duration::from_secs(
                 crate::config::DEFAULT_SUBAGENT_API_TIMEOUT_SECS,
             ),
+            exec_policy_engine: codewhale_execpolicy::ExecPolicyEngine::default(),
         }
     }
 }
@@ -1999,10 +2003,11 @@ use self::approval::{ApprovalDecision, ApprovalResult, UserInputDecision};
 use self::dispatch::should_parallelize_tool_batch;
 use self::dispatch::{
     ParallelToolResult, ParallelToolResultEntry, ToolExecGuard, ToolExecOutcome,
-    ToolExecutionBatch, ToolExecutionPlan, caller_allowed_for_tool, caller_type_for_tool_use,
-    final_tool_input, format_tool_error, mcp_tool_approval_description, mcp_tool_is_parallel_safe,
-    mcp_tool_is_read_only, parse_parallel_tool_calls, parse_tool_input,
+    ToolExecutionBatch, ToolExecutionPlan, ToolPermissionOverride, caller_allowed_for_tool,
+    caller_type_for_tool_use, final_tool_input, format_tool_error, mcp_tool_approval_description,
+    mcp_tool_is_parallel_safe, mcp_tool_is_read_only, parse_parallel_tool_calls, parse_tool_input,
     plan_tool_execution_batches, should_force_update_plan_first, should_stop_after_plan_tool,
+    tool_permission_override_for_call,
 };
 use self::loop_guard::{AttemptDecision, LoopGuard, OutcomeDecision};
 #[cfg(test)]

crates/tui/src/main.rs
@@ -8,6 +8,7 @@ use std::time::Duration;
 use anyhow::{Context, Result, anyhow, bail};
 use clap::{Args, CommandFactory, Parser, Subcommand, ValueEnum};
 use clap_complete::{Shell, generate};
+use codewhale_execpolicy::PermissionDecision;
 use dotenvy::dotenv;
 use tempfile::NamedTempFile;
 use wait_timeout::ChildExt;
@@ -4715,6 +4716,11 @@ fn merge_project_config(config: &mut Config, workspace: &Path) {
         config.allow_shell = Some(v);
     }
 
+    // Project-level permission config is allowed only when it tightens the
+    // effective policy. A repository can require extra prompts or denials, but
+    // it cannot grant itself broader tool access.
+    merge_project_permission_config(config, table);
+
     // #454: instructions array — project replaces user. Empty arrays
     // count: explicit `instructions = []` clears the user's list for
     // this repo, useful when the user has a verbose global file that
@@ -4728,6 +4734,72 @@ fn merge_project_config(config: &mut Config, workspace: &Path) {
             .collect();
         config.instructions = Some(entries);
     }
+}
+
+fn merge_project_permission_config(
+    config: &mut Config,
+    table: &toml::map::Map<String, toml::Value>,
+) {
+    if let Some(value) = table.get("auto_allow") {
+        let ignored = value.as_array().map_or(0, Vec::len);
+        if ignored > 0 {
+            eprintln!(
+                "warning: project-scope `auto_allow` is ignored — \
+                 project config cannot grant persistent command allow rules."
+            );
+        }
+    }
+
+    if let Some(value) = table.get("auto_deny") {
+        match string_array_from_project_value(value) {
+            Some(entries) => config.auto_deny.extend(entries),
+            None => eprintln!(
+                "warning: project-scope `auto_deny` is ignored — expected an array of strings."
+            ),
+        }
+    }
+
+    let Some(value) = table.get("permissions") else {
+        return;
+    };
+    let permissions: crate::config::PermissionsConfig = match value.clone().try_into() {
+        Ok(permissions) => permissions,
+        Err(err) => {
+            eprintln!(
+                "warning: project-scope `[permissions]` is ignored — failed to parse permission rules: {err}"
+            );
+            return;
+        }
+    };
+
+    for rule in permissions.rules {
+        match rule.decision {
+            PermissionDecision::Deny | PermissionDecision::Ask => {
+                config.permissions.rules.push(rule);
+            }
+            PermissionDecision::Allow => {
+                eprintln!(
+                    "warning: project-scope allow permission rule `{}` is ignored — \
+                     project config cannot grant persistent tool allow rules.",
+                    rule.pattern_label()
+                );
+            }
+        }
+    }
+}
+
+fn string_array_from_project_value(value: &toml::Value) -> Option<Vec<String>> {
+    Some(
+        value
+            .as_array()?
+            .iter()
+            .map(toml::Value::as_str)
+            .collect::<Option<Vec<_>>>()?
+            .into_iter()
+            .filter(|item| !item.trim().is_empty())
+            .map(str::to_string)
+            .collect(),
+    )
 }
 
 async fn run_interactive(
@@ -5155,6 +5227,7 @@ async fn run_exec_agent(
             .and_then(|s| s.provider)
             .unwrap_or_default(),
         search_api_key: config.search.as_ref().and_then(|s| s.api_key.clone()),
+        exec_policy_engine: config.exec_policy_engine(),
     };
 
     let engine_handle = spawn_engine(engine_config, config);
@@ -6356,6 +6429,48 @@ allow_shell = false
         merge_project_config(&mut config, tmp.path());
         assert_eq!(config.max_subagents, Some(4));
         assert_eq!(config.allow_shell, Some(false));
+    }
+
+    #[test]
+    fn project_overlay_applies_only_tightening_permission_rules() {
+        let tmp = workspace_with_project_config(
+            r#"
+auto_allow = ["cargo test"]
+auto_deny = ["rm -rf"]
+
+[[permissions.rules]]
+tool = "read_file"
+decision = "ask"
+path = "secrets/**"
+
+[[permissions.rules]]
+tool = "write_file"
+decision = "deny"
+path = "src/**"
+
+[[permissions.rules]]
+tool = "exec_shell"
+decision = "allow"
+command = "cargo test"
+"#,
+        );
+        let mut config = Config::default();
+        merge_project_config(&mut config, tmp.path());
+
+        assert!(
+            config.auto_allow.is_empty(),
+            "project-scope auto_allow must not grant access"
+        );
+        assert_eq!(config.auto_deny, ["rm -rf"]);
+        assert_eq!(config.permissions.rules.len(), 2);
+        assert_eq!(
+            config.permissions.rules[0].decision,
+            PermissionDecision::Ask
+        );
+        assert_eq!(
+            config.permissions.rules[1].decision,
+            PermissionDecision::Deny
+        );
     }
 
     #[test]